f468c5226e4f63017cd47da5daa91ec37b791142
YongLeeCode <yonghwan311@gmail.com>
2025-03-31 18:05:40 +0900

    feat: user 도메인에 패스워드 필드 추가

--- src/main/java/com/schedule/user/repository/UserRepository.java
+++ src/main/java/com/schedule/user/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.schedule.user.repository;
 import com.schedule.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * @author : yong
  * @packageName : com.schedule.user.repository
@@ -11,4 +13,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @description :
  */
 public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findByPassword(String password);
 }
