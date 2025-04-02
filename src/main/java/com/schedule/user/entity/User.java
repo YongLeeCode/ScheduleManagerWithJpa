28931d5143b8b9ba9a5757c361eaf145091bfc10
YongLeeCode <yonghwan311@gmail.com>
2025-04-02 17:31:58 +0900

    feat: 이메일 유니크 적용

--- src/main/java/com/schedule/user/entity/User.java
+++ src/main/java/com/schedule/user/entity/User.java
@@ -1,10 +1,12 @@
 package com.schedule.user.entity;
 
 import com.schedule.common.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Getter;
+import org.hibernate.validator.constraints.UniqueElements;
 
 import java.sql.Timestamp;
 
@@ -21,8 +23,12 @@ public class User extends BaseEntity {
     @Id
     @GeneratedValue
     private long id;
+
     private String name;
+
+    @Column(unique = true)
     private String email;
+
     private String password;
 
     public User() {}
